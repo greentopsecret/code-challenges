53829607a93717451d94c8393af63c01428037c7
greentopsecret <greentopsecret@gmail.com>
2022-01-05 14:13:31 +0200

    min_length_substring - fixes

--- facebook/minimum_length_substrings.py
+++ facebook/minimum_length_substrings.py
@@ -1,4 +1,27 @@
 from collections import defaultdict
+import math
+
+from collections import Counter
+import math
+
+
+def min_window_alt(s, t):  # easier to follow
+    res = math.inf
+    l, r = 0, 0
+    d = Counter(t)
+    l_d = 0
+    while r < len(s):
+        d[s[r]] -= 1
+        if d[s[r]] >= 0:
+            l_d += 1
+        while l_d == len(t):
+            res = min(res, r - l + 1)
+            d[s[l]] += 1
+            if d[s[l]] > 0:
+                l_d -= 1
+            l += 1
+        r += 1
+    return res if res != math.inf else -1
 
 
 # s=dcbefedce
@@ -7,24 +30,25 @@ def min_length_substring(s, t):
     if not len(s) or not len(t):
         return False
 
-    freq = defaultdict(int)
-    for char in t:
-        freq[char] += 1
+    counter = Counter(t)
 
     l = 0
-    ml = 1000001
+    sub_l = 0
+    ml = math.inf
     for r in range(len(s)):
         char = s[r]
-        if char in freq.keys():
-            freq[char] -= 1
+        counter[char] -= 1
+        if counter[char] >= 0:
+            sub_l += 1
 
-        while l <= r and max(freq.values()) == 0:
+        while l <= r and sub_l == len(t):
             ml = min(ml, r - l + 1)
-            if s[l] in freq.keys():
-                freq[s[l]] += 1
+            counter[s[l]] += 1
+            if counter[s[l]] > 0:
+                sub_l -= 1
             l += 1
 
-    return -1 if ml == 1000001 else ml
+    return -1 if ml == math.inf else ml
 
 
 # These are the tests we use to determine if the solution is correct.
@@ -56,19 +80,21 @@ def check(expected, output):
 
 
 if __name__ == "__main__":
-    s1 = "dcbefebce"
-    t1 = "fd"
-    expected_1 = 5
-    output_1 = min_length_substring(s1, t1)
-    check(expected_1, output_1)
+    # s1 = "dcbefebce"
+    # t1 = "fd"
+    # expected_1 = 5
+    # output_1 = min_length_substring(s1, t1)
+    # check(expected_1, output_1)
+    #
+    # s2 = "bfbeadbcbcbfeaaeefcddcccbbbfaaafdbebedddf"
+    # t2 = "cbccfafebccdccebdd"
+    # expected_2 = -1
+    # output_2 = min_length_substring(s2, t2)
+    # check(expected_2, output_2)
 
-    s2 = "bfbeadbcbcbfeaaeefcddcccbbbfaaafdbebedddf"
-    t2 = "cbccfafebccdccebdd"
-    expected_2 = -1
-    output_2 = min_length_substring(s2, t2)
-    check(expected_2, output_2)
-
-    check(3, min_length_substring("dcbefedce", "fd"))
-    check(7, min_length_substring("dcbefedce", "dfd"))
+    check(3, min_length_substring("dddcbefedce", "fd"))
+    check(3, min_window_alt("dddcbefedce", "fd"))
+    # check(3, min_length_substring("dcbefedce", "fd"))
+    # check(7, min_length_substring("dcbefedce", "dfd"))
 
     # Add your own test cases here
