ec82d1e1575f863f2fb417f7ab83cf14807fd9d6
greentopsecret <greentopsecret@gmail.com>
2022-01-06 16:55:55 +0200

    is_bst - one more solution

--- cracking_the_coding_interview/Chapter4/4.5-is_bst.py
+++ cracking_the_coding_interview/Chapter4/4.5-is_bst.py
@@ -43,6 +43,19 @@ def _is_bst_a(node: TreeNode):
     return True, min(left_min_value, right_min_value, node.value), max(left_max_value, right_max_value, node.value)
 
 
+def is_bst_b(node: TreeNode, min_val: int = None, max_val: int = None):
+    if node is None:
+        return True
+
+    if min_val and min_val >= node.value:
+        return False
+
+    if max_val and max_val < node.value:
+        return False
+
+    return is_bst_b(node.left, min_val, node.value) and is_bst_b(node.right, node.value, max_val)
+
+
 def is_bst_c(node: TreeNode):
     result, _ = _is_bst_c(node, None)
     return result
@@ -71,26 +84,32 @@ if __name__ == '__main__':
     def main():
         root = build_bst([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13])
         assert is_bst_a(root)
+        assert is_bst_b(root)
         assert is_bst_c(root)
 
         root = build_bst([2, 1, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13])
         assert not is_bst_a(root)
+        assert not is_bst_b(root)
         assert not is_bst_c(root)
 
         root = build_bst([1, 2, 3, 4, 5, 6, 7])
         assert is_bst_a(root)
+        assert is_bst_b(root)
         assert is_bst_c(root)
 
         root = build_bst([1, 2, 3, 4, 6, 6, 7])
         assert is_bst_a(root)
+        assert is_bst_b(root)
         assert is_bst_c(root)
 
         root = build_bst([1, 2, 3, 4, 5, 6, 6])
         assert not is_bst_a(root)
+        assert not is_bst_b(root)
         assert not is_bst_c(root)
 
         root = build_bst([1, 2, 5, 4, 5, 6, 7])
         assert not is_bst_a(root)
+        assert not is_bst_b(root)
         assert not is_bst_c(root)
 
         print('All tests are passed')
