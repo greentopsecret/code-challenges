02c969f2260a5093cc41380cb589fb5870eaebee
greentopsecret <greentopsecret@gmail.com>
2022-01-11 13:07:16 +0200

    5.3 - bit manipulations - flip_to_win - correct solution

--- cracking_the_coding_interview/Chapter5/5.3-flip_to_win.py
+++ cracking_the_coding_interview/Chapter5/5.3-flip_to_win.py
@@ -1,25 +1,33 @@
 import math
 
 
-# 110111001111
-def flip_to_win(num: int) -> int:
+def flip_to_win(num: int):
+    if num == -1:
+        return math.inf
     sequence = 0
     prev_sequence = 0
+    prev_prev_sequence = 0
     max_len = 0
+    current_bit = 0
     while num > 0:
-        bit = num % 2
-        num = math.floor(num / 2)
-        if bit:
-            sequence += 1
-        else:
-            max_len = max(max_len, sequence + prev_sequence + 1)
+        if (num & 1) != current_bit:
+            if prev_sequence == 1:
+                max_len = max(max_len, prev_prev_sequence + sequence + 1)
+            elif prev_sequence > 1:
+                max_len = max(max_len, prev_sequence + 1)
+            prev_prev_sequence = prev_sequence
             prev_sequence = sequence
             sequence = 0
+            current_bit = num & 1
+        sequence += 1
+        num >>= 1
 
-    if prev_sequence > 0:
-        max_len = max(max_len, sequence + prev_sequence + 1)
-    else:
-        max_len = sequence
+    if prev_sequence == 1:
+        max_len = max(max_len, prev_prev_sequence + sequence + 1)
+    elif prev_sequence > 1:
+        max_len = max(max_len, max(sequence, prev_sequence) + 1)
+    elif prev_sequence < 1:
+        max_len = max(max_len, sequence)
 
     return max_len
 
@@ -28,7 +36,11 @@ if __name__ == '__main__':
     def main():
         assert flip_to_win(1775) == 8  # 11011101111
         assert flip_to_win(3535) == 6  # 110111001111
+        assert flip_to_win(28) == 4  # 11100
+        assert flip_to_win(24) == 4  # 11000
         assert flip_to_win(15) == 4  # 1111
+        assert flip_to_win(-1) == math.inf  # ...11111111111
+        # assert flip_to_win(-11) == 6  # 11110101
 
         print('All tests are passed')
 
