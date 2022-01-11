03d13d72c1bc3191030dfe6c754cf099a89429e7
greentopsecret <greentopsecret@gmail.com>
2022-01-11 14:20:03 +0200

    5.3 - bit manipulations - flip_to_win - approach from the book

--- cracking_the_coding_interview/Chapter5/5.3-flip_to_win.py
+++ cracking_the_coding_interview/Chapter5/5.3-flip_to_win.py
@@ -2,29 +2,27 @@ import math
 
 
 # 11011101111
+# 110111001111
 def flip_to_win(num: int):
     if num == -1:
         return math.inf
-    sequence = 0
+
+    current_sequence = 0
     prev_sequence = 0
-    prev_prev_sequence = 0
     max_len = 0
-    current_bit = 0
+    bits_cnt = 0
     while num > 0:
-        if (num & 1) != current_bit:
-            prev_prev_sequence = prev_sequence
-            prev_sequence = sequence
-            sequence = 0
-            current_bit = num & 1
+        bits_cnt += 1
+        if (num & 1) == 1:
+            current_sequence += 1
+        else:
+            prev_sequence = 0 if (num & 2) == 0 else current_sequence
+            current_sequence = 0
 
-        sequence += 1
-
-        if prev_sequence == 1:
-            max_len = max(max_len, prev_prev_sequence + sequence + 1)
-        elif prev_sequence == 0:
-            max_len = max(max_len, sequence)
-        elif prev_sequence > 1:
-            max_len = max(max_len, max(sequence, prev_sequence) + 1)
+        if current_sequence == bits_cnt:
+            max_len = max(max_len, current_sequence)
+        else:
+            max_len = max(max_len, prev_sequence + current_sequence + 1)
 
         num >>= 1
 
@@ -33,10 +31,11 @@ def flip_to_win(num: int):
 
 if __name__ == '__main__':
     def main():
+        assert flip_to_win(91) == 5  # 1011011
         assert flip_to_win(1775) == 8  # 11011101111
         assert flip_to_win(3535) == 6  # 110111001111
         assert flip_to_win(28) == 4  # 11100
-        assert flip_to_win(24) == 4  # 11000
+        assert flip_to_win(24) == 3  # 11000
         assert flip_to_win(15) == 4  # 1111
         assert flip_to_win(-1) == math.inf  # ...11111111111
         # assert flip_to_win(-11) == 6  # 11110101
