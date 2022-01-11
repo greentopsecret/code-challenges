03d9117bb3211ccd0c350c1dda5dab4d4c7a035a
greentopsecret <greentopsecret@gmail.com>
2022-01-11 17:01:19 +0200

    5.4 - get next smaller/bigger - bit solution

--- cracking_the_coding_interview/Chapter5/5.4-next_numbers.py
+++ cracking_the_coding_interview/Chapter5/5.4-next_numbers.py
@@ -17,15 +17,71 @@ import math
 # 1101  13
 # 1110  14
 # 1111  15
+
+# 0101  5   =>  0011  3
 def next_smallest_number(num: int):
+    # find the first non-trailing one
+    p = 0
+    n = num
+    while n > 0 and (n & 1) == 1:
+        p += 1
+        n >>= 1
+
+    if n == 0:  # only ones in the input number
+        return None
+
+    while n > 0 and (n & 1) == 0:
+        p += 1
+        n >>= 1
+
+    # set zero bit at "p" position
+    num &= ~(1 << p)
+
+    # set one bit at "p-1" position
+    num |= 1 << (p - 1)
+
+    return num
+
+
+def next_biggest_number(num: int):
+    if num == 0:
+        return None
+
+    # count number of ones and zeros before first non-trailing zero
+    c0 = c1 = 0
+    c = num
+    while c > 0 and (c & 1) == 0:
+        c0 += 1
+        c >>= 1
+
+    while c > 0 and (c & 1) == 1:
+        c1 += 1
+        c >>= 1
+
+    # set one into the first non-trailing zero
+    p = c0 + c1
+    num |= 1 << p
+
+    # put zeros to all bits on the right from "p"
+    mask = ~((1 << p) - 1)
+    num &= mask
+
+    # put c1-1 ones to the right from "p"
+    mask = (1 << (c1 - 1)) - 1
+    num |= mask
+
+    return num
+
+
+def next_smallest_number_bruteforce(num: int):
     ones_cnt = get_ones_cnt(num)
     _next = num - 1
     while _next > 0 and get_ones_cnt(_next) != ones_cnt:
         _next = _next - 1
-    return _next
+    return _next if _next > 0 else None
 
 
-def next_biggest_number(num: int):
+def next_biggest_number_bruteforce(num: int):
     ones_cnt = get_ones_cnt(num)
     _next = num + 1
     while get_ones_cnt(_next) != ones_cnt:
@@ -50,6 +106,8 @@ if __name__ == '__main__':
 
         assert next_smallest_number(11) == 7
         assert next_biggest_number(11) == 13
+
+        assert next_smallest_number(7) is None
         print('All tests are passed')
 
 
