11261ecdd4068d756bd4abdd2aa016ac212bd558
greentopsecret <greentopsecret@gmail.com>
2022-01-11 19:26:06 +0200

    5.4 - get next smaller/bigger - final version

--- cracking_the_coding_interview/Chapter5/5.4-next_numbers.py
+++ cracking_the_coding_interview/Chapter5/5.4-next_numbers.py
@@ -1,42 +1,31 @@
-import math
-
-
-# 00000  0
-# 00001  1
-# 00010  2
-# 00011  3
-# 00100  4
-# 00101  5
-# 00110  6
-# 00111  7
-# 01000  8
-# 01001  9
-# 01010  10
-# 01011  11
-# 01100  12
-# 01101  13
-# 01110  14
-# 01111  15
-# 10000  16
-# 10001  17
-# 10010  18
-# 10011  19
-# 10100  20
-# 10101  21
-# 10110  22
-# 10111  23
-# 11000  24
-# 11001  25
-# 11010  26
-# 11011  27
-# 11100  28
-# 11101  29
-# 11110  30
-# 11111  31
-
 # 0101  5   =>  0011  3
 # 10011 => 01110
 def next_smaller_number(num: int):
+    # return next_smaller_number_via_bit_manipulation(num)
+
+    # find the first non-trailing one
+    c0 = c1 = 0
+    n = num
+    while n > 0 and (n & 1) == 1:
+        c1 += 1
+        n >>= 1
+
+    if n == 0:  # only ones in the input number
+        return None
+
+    while n > 0 and (n & 1) == 0:
+        c0 += 1
+        n >>= 1
+
+    # # 10000011 => 01110000
+    # num -= (1 << c1) - 1       # 10000000
+    # num -= 1                   # 01111111
+    # num -= (1 << (c0 - 1)) - 1 # 01110000
+
+    return num - (1 << c1) - (1 << (c0 - 1)) + 1
+
+
+def next_smaller_number_via_bit_manipulation(num: int):
     # find the first non-trailing one
     c0 = c1 = 0
     n = num
@@ -68,6 +57,30 @@ def next_smaller_number(num: int):
 
 
 def next_bigger_number(num: int):
+    # return next_bigger_number_via_bit_manipulation(num)
+    if num == 0:
+        return None
+
+    # count number of ones and zeros before first non-trailing zero
+    c0 = c1 = 0
+    c = num
+    while c > 0 and (c & 1) == 0:
+        c0 += 1
+        c >>= 1
+
+    while c > 0 and (c & 1) == 1:
+        c1 += 1
+        c >>= 1
+
+    # num += pow(2, c0) - 1      # set all trailing zeros as ones
+    # num += 1                   # clear all bits from right from first non-trailing zero
+    # num += pow(2, c1 - 1) - 1  # add "c1-1" ones
+    # return num
+
+    return num + (1 << c0) + (1 << c1 - 1) - 1
+
+
+def next_bigger_number_via_bit_manipulation(num: int):
     if num == 0:
         return None
 
@@ -127,14 +140,30 @@ if __name__ == '__main__':
     def main():
         assert next_bigger_number(6) == 9
         assert next_bigger_number(11) == 13
+        assert next_bigger_number(78) == 83  # 1001110 => 1010011
+        assert next_bigger_number(43) == 45  # 101011 => 101101
+        # print(next_bigger_number(43))
 
         assert next_smaller_number(7) is None
-        assert next_smaller_number(6) == 5
+        assert next_smaller_number(6) == 5  # 110 => 101
+        assert next_smaller_number(131) == 112  # 10000011 => 01110000
         assert next_smaller_number(19) == 14  # 10011 => 01110
         assert next_smaller_number(10115) == 10096  # 10011110000011 => 10011101110000
         assert next_smaller_number(11) == 7
         assert next_smaller_number(21) == 19
+        assert next_smaller_number(38) == 37  # 100110 => 100101
+        assert next_smaller_number(78) == 77  # 1001110 => 1001101
+        # print(next_smaller_number(78))
         print('All tests are passed')
 
+
+    # 1001110 = > 1010011
+    # 1011110
+    # 1010000
+    # 1010011
+
+    # 1001110
+    # 1001111
+    # 1010000
 
     main()
