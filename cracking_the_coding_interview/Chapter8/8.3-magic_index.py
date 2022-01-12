ff3fe02736fd7cd101e2a44f626e8abd87022eb3
greentopsecret <greentopsecret@gmail.com>
2022-01-12 17:07:56 +0200

    recursion and dp: 8.3 magic-index - solution for non-unique items

--- cracking_the_coding_interview/Chapter8/8.3-magic_index.py
+++ cracking_the_coding_interview/Chapter8/8.3-magic_index.py
@@ -3,8 +3,36 @@ from typing import Union
 
 
 def solution(arr: list) -> Union[int, None]:
-    # return solution_recursive(arr, 0, len(arr) - 1)
-    return solution_iterative_unique(arr)
+    # return solution_recursive_unique(arr, 0, len(arr) - 1)
+    # return solution_iterative_unique(arr)
+
+    res = solution_recursive_non_unique(arr, 0, len(arr) - 1)
+    return None if res is False else res
+
+
+def solution_recursive_non_unique(arr, start, end) -> Union[int, bool]:
+    if end < start:
+        return False
+
+    mid = math.floor((end + start) / 2)
+    mid_value = arr[mid]
+    if mid == mid_value:
+        return mid
+
+    if mid_value > mid:
+        res = solution_recursive_non_unique(arr, start, mid - 1)
+        if res:
+            return res
+        if mid_value > 0 and mid_value < len(arr):
+            return solution_recursive_non_unique(arr, mid_value, end)
+
+    if mid_value < mid:
+        res = solution_recursive_non_unique(arr, mid + 1, end)
+        if res:
+            return res
+
+        if mid_value > 0 and mid_value < len(arr):
+            return solution_recursive_non_unique(arr, start, mid_value)
 
 
 def solution_iterative_unique(arr) -> Union[int, None]:
@@ -15,32 +43,41 @@ def solution_iterative_unique(arr) -> Union[int, None]:
     :param arr:
     :return:
     """
-    i = 0
-    j = len(arr) - 1
-    while i <= j:
-        mid = math.floor((i + j) / 2)
-        if mid == arr[mid]:
+    start = 0
+    end = len(arr) - 1
+    while start <= end:
+        mid = math.floor((start + end) / 2)
+        mid_value = arr[mid]
+        if mid == mid_value:
             return mid
-        if arr[mid] > mid:
-            j = mid - 1
-        if arr[mid] < mid:
-            i = mid + 1
+        if mid_value > mid:
+            end = mid - 1
+        if mid_value < mid:
+            start = mid + 1
     return None
 
 
-def solution_recursive(arr, i, j) -> Union[int, None]:
-    if j < i:
+def solution_recursive_unique(arr, start, end) -> Union[int, None]:
+    """
+
+    this solution works only for unique elements
+
+    :param arr:
+    :return:
+    """
+    if end < start:
         return None
 
-    mid = math.floor((j + i) / 2)
-    if mid == arr[mid]:
+    mid = math.floor((end + start) / 2)
+    mid_value = arr[mid]
+    if mid == mid_value:
         return mid
 
-    if arr[mid] > mid:
-        return solution_recursive(arr, i, mid - 1)
+    if mid_value > mid:
+        return solution_recursive_unique(arr, start, mid - 1)
 
-    if arr[mid] < mid:
-        return solution_recursive(arr, mid + 1, j)
+    if mid_value < mid:
+        return solution_recursive_unique(arr, mid + 1, end)
 
 
 if __name__ == '__main__':
@@ -48,6 +85,7 @@ if __name__ == '__main__':
         assert solution([1, 2, 3]) is None
         assert solution([-5, -3, -1, 1, 4, 6, 8]) == 4
         assert solution([-40, -20, -1, 1, 2, 3, 5, 7, 9, 12, 13]) == 7
+        assert solution([-10, -5, 2, 2, 2, 3, 4, 7, 9, 12, 13]) == 7
 
         # print(solution([1, 2, 3]))
 
