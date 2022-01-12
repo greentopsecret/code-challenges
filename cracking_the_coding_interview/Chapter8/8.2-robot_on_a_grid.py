bbd8985f69eeb6767c36562ebc0359ecf979c517
greentopsecret <greentopsecret@gmail.com>
2022-01-12 15:46:13 +0200

    recursion and dp: 8.2 robot-in-a-grid - memoization

--- cracking_the_coding_interview/Chapter8/8.2-robot_on_a_grid.py
+++ cracking_the_coding_interview/Chapter8/8.2-robot_on_a_grid.py
@@ -1,12 +1,12 @@
-from collections import deque
+from collections import deque, defaultdict
 
 
 def solution(maze) -> int:
-    # return solution_recursive(len(maze[0]) - 1, len(maze) - 1, maze)
-    return solution_dp(maze)
+    return solution_recursive(len(maze[0]) - 1, len(maze) - 1, maze, defaultdict(int))
+    # return solution_via_stack(maze)
 
 
-def solution_dp(maze) -> int:
+def solution_via_stack(maze) -> int:
     c = len(maze[0]) - 1
     r = len(maze) - 1
 
@@ -26,17 +26,19 @@ def solution_dp(maze) -> int:
     return cnt
 
 
-def solution_recursive(c, r, maze) -> int:
-    if r == 0 and c == 0:
+def solution_recursive(c, r, maze, cache: dict) -> int:
+    if c == 0 and r == 0:
         return 1
 
-    cnt = 0
-    if c - 1 >= 0 and maze[r][c - 1]:
-        cnt += solution_recursive(c - 1, r, maze)
-    if r - 1 >= 0 and maze[r - 1][c]:
-        cnt += solution_recursive(c, r - 1, maze)
+    if (c, r) not in cache.keys():
+        cnt = 0
+        if c - 1 >= 0 and maze[r][c - 1]:
+            cnt += solution_recursive(c - 1, r, maze, cache)
+        if r - 1 >= 0 and maze[r - 1][c]:
+            cnt += solution_recursive(c, r - 1, maze, cache)
+        cache[(c, r)] = cnt
 
-    return cnt
+    return cache[(c, r)]
 
 
 if __name__ == '__main__':
@@ -92,6 +94,77 @@ if __name__ == '__main__':
             [False, True, True],
         ]
         assert solution(maze) == 0
+
+        maze = [
+            [True, True, True],
+            [True, True, True],
+            [True, True, True],
+        ]
+        solution(maze)
+
+        maze = [
+            [True, True, True, True],
+            [True, True, True, True],
+            [True, True, True, True],
+            [True, True, True, True],
+        ]
+        assert solution(maze) == 20
+
+        maze = [
+            [True, True, True, True, True],
+            [True, True, True, True, True],
+            [True, True, True, True, True],
+            [True, True, True, True, True],
+            [True, True, True, True, True],
+        ]
+        assert solution(maze) == 70
+
+        maze = [
+            [True, True, True, True, True, True, True, True, True, True],
+            [True, True, True, True, True, True, True, True, True, True],
+            [True, True, True, True, True, True, True, True, True, True],
+            [True, True, True, True, True, True, True, True, True, True],
+            [True, True, True, True, True, True, True, True, True, True],
+            [True, True, True, True, True, True, True, True, True, True],
+            [True, True, True, True, True, True, True, True, True, True],
+            [True, True, True, True, True, True, True, True, True, True],
+            [True, True, True, True, True, True, True, True, True, True],
+            [True, True, True, True, True, True, True, True, True, True],
+        ]
+        assert solution(maze) == 48620
+
+        maze = [
+            [True, True, True, True, True, True, True, True, True, True, True, True],
+            [True, True, True, True, True, True, True, True, True, True, True, True],
+            [True, True, True, True, True, True, True, True, True, True, True, True],
+            [True, True, True, True, True, True, True, True, True, True, True, True],
+            [True, True, True, True, True, True, True, True, True, True, True, True],
+            [True, True, True, True, True, True, True, True, True, True, True, True],
+            [True, True, True, True, True, True, True, True, True, True, True, True],
+            [True, True, True, True, True, True, True, True, True, True, True, True],
+            [True, True, True, True, True, True, True, True, True, True, True, True],
+            [True, True, True, True, True, True, True, True, True, True, True, True],
+            [True, True, True, True, True, True, True, True, True, True, True, True],
+            [True, True, True, True, True, True, True, True, True, True, True, True],
+        ]
+        assert solution(maze) == 705432
+
+        maze = [
+            [True, True, True, True, True, True, True, True, True, True, True, True, True],
+            [True, True, True, True, True, True, True, True, True, True, True, True, True],
+            [True, True, True, True, True, True, True, True, True, True, True, True, True],
+            [True, True, True, True, True, True, True, True, True, True, True, True, True],
+            [True, True, True, True, True, True, True, True, True, True, True, True, True],
+            [True, True, True, True, True, True, True, True, True, True, True, True, True],
+            [True, True, True, True, True, True, True, True, True, True, True, True, True],
+            [True, True, True, True, True, True, True, True, True, True, True, True, True],
+            [True, True, True, True, True, True, True, True, True, True, True, True, True],
+            [True, True, True, True, True, True, True, True, True, True, True, True, True],
+            [True, True, True, True, True, True, True, True, True, True, True, True, True],
+            [True, True, True, True, True, True, True, True, True, True, True, True, True],
+            [True, True, True, True, True, True, True, True, True, True, True, True, True],
+        ]
+        assert solution(maze) == 2704156
 
         print('All tests are passed')
 
