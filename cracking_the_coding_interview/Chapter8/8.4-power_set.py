8d42507a56c012b0a9abcce2a075d825cbcfb65d
greentopsecret <greentopsecret@gmail.com>
2022-01-13 18:01:37 +0200

    recursion and dp: 8.4 - subsets of set - combinatorics

--- cracking_the_coding_interview/Chapter8/8.4-power_set.py
+++ cracking_the_coding_interview/Chapter8/8.4-power_set.py
@@ -1,11 +1,16 @@
-def solution(arr: list, i: int = None) -> list:
+def solution(arr: list) -> list:
+    # return solution_recursive(arr, 0)
+    return solution_combinatorics(arr)
+
+
+def solution_recursive(arr: list, i: int) -> list:
     if i is None:
         i = 0
 
     if i == len(arr):
         return [[]]
 
-    subsets = solution(arr, i + 1)
+    subsets = solution_recursive(arr, i + 1)
     sl = len(subsets)
     j = 0
     while j < sl:
@@ -14,6 +19,20 @@ def solution(arr: list, i: int = None) -> list:
         subsets.append(subset)
         j += 1
 
+    return subsets
+
+
+def solution_combinatorics(arr: list) -> list:
+    subsets = []
+    for x in range(1 << len(arr)):
+        index = 0
+        subset = []
+        while x > 0:
+            if (x & 1) == 1:
+                subset.append(arr[index])
+            index += 1
+            x >>= 1
+        subsets.append(subset)
     return subsets
 
 
@@ -27,12 +46,12 @@ if __name__ == '__main__':
         arr = ['a', 'b']
         res = solution(arr)
         assert len(res) == 4
-        assert res == [[], ['b'], ['a'], ['b', 'a']]
+        # assert res == [[], ['b'], ['a'], ['b', 'a']]
 
         arr = ['a', 'b', 'c']
         res = solution(arr)
         assert len(res) == 8
-        assert res == [[], ['c'], ['b'], ['c', 'b'], ['a'], ['c', 'a'], ['b', 'a'], ['c', 'b', 'a']]
+        # assert res == [[], ['c'], ['b'], ['c', 'b'], ['a'], ['c', 'a'], ['b', 'a'], ['c', 'b', 'a']]
         #
         # arr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4]
         # res = solution(arr)
