006e2c640ec30e8bb3378c91e4088f0ff6657fd8
greentopsecret <greentopsecret@gmail.com>
2022-01-14 15:08:01 +0200

    cci: hard exercises: 17.1 - adding - elegant solution from the book

--- cracking_the_coding_interview/Chapter17/17.1-adding.py
+++ cracking_the_coding_interview/Chapter17/17.1-adding.py
@@ -1,17 +1,23 @@
 def solution(a: int, b: int) -> int:
+    # return solution_iterative_a(a, b)
+    # return solution_iterative_b(a, b)
+    return solution_recursive(a, b)
+
+
+def solution_iterative_a(a: int, b: int) -> int:
     result = 0
-    rem = 0
+    carry = 0
     setter = 1
 
     # 0011
     # 0011
     # ----
     # 0110
-    while a > 0 or b > 0 or rem > 0:
+    while a > 0 or b > 0 or carry > 0:
         a_bit = a & 1
         b_bit = b & 1
-        s = a_bit ^ b_bit ^ rem
-        rem = (a_bit & b_bit) | (a_bit & rem) | (b_bit & rem)
+        s = a_bit ^ b_bit ^ carry
+        carry = (a_bit & b_bit) | (a_bit & carry) | (b_bit & carry)
         if s:
             result |= setter
         setter <<= 1
@@ -21,12 +27,30 @@ def solution(a: int, b: int) -> int:
     return result
 
 
+def solution_iterative_b(a: int, b: int) -> int:
+    result = 0
+
+    while b > 0:
+        result = a ^ b
+        carry = (a & b) << 1
+        a = result
+        b = carry
+
+    return result
+
+
+def solution_recursive(a: int, b: int) -> int:
+    if b <= 0:
+        return a
+    return solution_recursive(a ^ b, (a & b) << 1)
+
+
 if __name__ == '__main__':
     def main():
         assert solution(3, 3) == 6
         assert solution(1, 3) == 4
         assert solution(0, 3) == 3
-        assert solution(12, 35) == 47
+        assert solution(22, 27) == 49
 
         # print(solution([1, 2, 3]))
 
